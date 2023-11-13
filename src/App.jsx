400665311724a18e2fb7aeee17509b4932dd87c2
Daniel Diaz <danieldiaz48@gmail.com>
2023-11-13 12:58:19 +0100

    Deployment

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
+import Header from './components/Header.jsx';
+import Login from './components/StateLogin.jsx';
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
